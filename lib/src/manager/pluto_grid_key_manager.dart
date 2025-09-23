92158c01ff7f29183599fbcc5b3e40fbd352810a
kkn8dev <144164674+kkn8dev@users.noreply.github.com>
2025-09-23 14:38:14 +0700

    Update pluto_grid_key_manager.dart

--- lib/src/manager/pluto_grid_key_manager.dart
+++ lib/src/manager/pluto_grid_key_manager.dart
@@ -66,7 +66,7 @@ class PlutoGridKeyManager {
     final movingStream =
         _subject.stream.where((event) => event.needsThrottle).transform(
               ThrottleStreamTransformer(
-                (_) => TimerStream(_, const Duration(milliseconds: 1)),
+                (x) => TimerStream(x, const Duration(milliseconds: 1)),
               ),
             );
 
